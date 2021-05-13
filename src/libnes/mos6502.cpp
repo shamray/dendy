ba6aaa1609c5661c8719734ca0d5289459c174b3
Volodymyr Shamray <shamray@outlook.com>
2021-05-13 08:55:56 +0300

    Implement ZPX addressing mode

--- src/libnes/mos6502.cpp
+++ src/libnes/mos6502.cpp
@@ -3,9 +3,20 @@
 #include <cassert>
 #include <stdexcept>
 #include <unordered_map>
+#include <string>
+
+using namespace std::string_literals;
 
 namespace nes
 {
+
+class unsupported_opcode : public std::runtime_error
+{
+public:
+    unsupported_opcode(uint8_t opcode)
+        : std::runtime_error("Unsupported opcode: "s + std::to_string(opcode))
+    {}
+};
 
 namespace
 {
@@ -40,6 +51,11 @@ auto zp =  [](mos6502& cpu) {
     return cpu.read(address);
 };
 
+auto zpx = [](mos6502& cpu) {
+    auto address = cpu.read(cpu.pc++);
+    return cpu.read(cpu.x + address);
+};
+
 auto imp = [](mos6502& ) -> uint8_t {
     throw std::logic_error("Calling operand function for implied addressing mode");
 };
@@ -48,11 +64,14 @@ auto imp = [](mos6502& ) -> uint8_t {
 // Instruction set lookup table
 
 const std::unordered_map<uint8_t, mos6502::instruction> instruction_set {
+
     {0xA9, { lda, imm, 2 }},
     {0xA5, { lda, zp , 3 }},
+    {0xB5, { lda, zpx, 4 }},
 
     {0xA2, { ldx, imm, 2 }},
     {0xA6, { ldx, zp , 3 }},
+
     {0x00, { brk, imp, 7 }}
 };
 
@@ -65,7 +84,12 @@ mos6502::mos6502(std::vector<uint8_t>& memory)
 void mos6502::tick()
 {
     auto opcode = read(pc++);
-    auto cmd = decode(opcode).value_or(instruction{});
+    auto cmd = decode(opcode)
+        .value_or(instruction{
+            [opcode](auto...) { throw unsupported_opcode(opcode); },
+            imp,
+            0
+        });
     cmd.op(*this, cmd.arg);
 }
 
--- test/cpu.cpp
+++ test/cpu.cpp
@@ -31,10 +31,18 @@ TEST_CASE_METHOD(fixture, "LDA-IMM")
 {
     nes::mos6502 cpu(mem);
 
-    load(0x8000, std::array{0xa9, 0x55});
+    load(0x8000, std::array{0xa9, 0x55}); // LDA #$55
     cpu.tick();
 
     CHECK(cpu.a == 0x55);
+}
+
+TEST_CASE_METHOD(fixture, "Unsupported opcode")
+{
+    nes::mos6502 cpu(mem);
+
+    load(0x8000, std::array{ 0x02 });
+    CHECK_THROWS_AS(cpu.tick(), std::runtime_error);
 }
 
 TEST_CASE_METHOD(fixture, "LDA-ZP")
@@ -42,17 +50,29 @@ TEST_CASE_METHOD(fixture, "LDA-ZP")
     nes::mos6502 cpu(mem);
 
     load(0x0010, std::array{ 0x42 });
-    load(0x8000, std::array{ 0xa5, 0x10 });
+    load(0x8000, std::array{ 0xa5, 0x10 }); // LDA $10
     cpu.tick();
 
     CHECK(cpu.a == 0x42);
+}
+
+TEST_CASE_METHOD(fixture, "LDA-ZPX")
+{
+    nes::mos6502 cpu(mem);
+
+    cpu.x = 0x02;
+    load(0x0012, std::array{ 0x89 });
+    load(0x8000, std::array{ 0xb5, 0x10 }); // LDA $10,X
+    cpu.tick();
+
+    CHECK(cpu.a == 0x89);
 }
 
 TEST_CASE_METHOD(fixture, "LDX-IMM")
 {
     nes::mos6502 cpu(mem);
 
-    load(0x8000, std::array{ 0xa2, 0x42 });
+    load(0x8000, std::array{ 0xa2, 0x42 }); // LDX #$42
     cpu.tick();
 
     CHECK(cpu.x == 0x42);
@@ -63,7 +83,7 @@ TEST_CASE_METHOD(fixture, "LDX-ZP")
     nes::mos6502 cpu(mem);
 
     load(0x0010, std::array{ 0x88 });
-    load(0x8000, std::array{ 0xa6, 0x10 });
+    load(0x8000, std::array{ 0xa6, 0x10 }); // LDX $10
     cpu.tick();
 
     CHECK(cpu.x == 0x88);
