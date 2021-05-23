5ad5073161924f744ec9a3630962e666f5757337
Volodymyr Shamray <shamray@outlook.com>
2021-05-23 12:54:13 +0300

    Try to render texture

--- src/dendy_sdl/main.cpp
+++ src/dendy_sdl/main.cpp
@@ -1,5 +1,9 @@
 #include <SDL2/SDL.h>
 
+SDL_Texture* screen;
+SDL_Window* window;
+SDL_Renderer* renderer;
+
 void main_loop() {
     for (;;) {
         SDL_Event e;
@@ -10,13 +14,20 @@ void main_loop() {
                 case SDL_KEYDOWN: break;
             }
         }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, screen, NULL, NULL);
+        SDL_RenderPresent(renderer);
     }
 }
 
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindow("hello, world!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 256 * 2, 240 * 2, 0);
+    window = SDL_CreateWindow("hello, world!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 256 * 2, 240 * 2, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    auto surface = SDL_LoadBMP("link.bmp");
+    screen = SDL_CreateTextureFromSurface(renderer, surface);
     main_loop();
 
     return 0;
