6ce89d5cdd699b2bf6505b12847a7e69b50823dd
Volodymyr Shamray <shamray@outlook.com>
2021-12-18 18:42:41 +0200

    Move name_table class to dedicated file

--- src/libnes/ppu.hpp
+++ src/libnes/ppu.hpp
@@ -2,12 +2,11 @@
 
 #include <libnes/literals.hpp>
 #include <libnes/color.hpp>
+#include <libnes/ppu_name_table.hpp>
 
 #include <array>
 #include <optional>
-#include <stdexcept>
 #include <cassert>
-#include <ranges>
 #include <algorithm>
 #include <unordered_map>
 
@@ -40,51 +39,8 @@ private:
     const memory_bank* bank_{nullptr};
 };
 
-enum class name_table_mirroring { horizontal, vertical };
 
-class name_table
-{
-public:
-    name_table_mirroring mirroring{name_table_mirroring::vertical};
 
-    constexpr void write(std::uint16_t addr, std::uint8_t value) {
-        auto i = bank_index(addr);
-        auto j = bank_offset(addr);
-
-        vram_[i][j] = value;
-    }
-    [[nodiscard]] constexpr auto read(std::uint16_t addr) const {
-        auto i = bank_index(addr);
-        auto j = bank_offset(addr);
-        return vram_[i][j];
-    }
-
-    [[nodiscard]] constexpr auto table(int bank) const -> auto& {
-        return vram_[bank & 1];
-    }
-
-    name_table() {
-        std::ranges::for_each(vram_, [](auto& x){ std::ranges::fill(x, 0); });
-    }
-
-private:
-    [[nodiscard]] constexpr auto bank_index(std::uint16_t addr) const -> int {
-        using enum name_table_mirroring;
-        switch(mirroring) {
-            case horizontal:    return (addr >> 11) & 0x01;
-            case vertical:      return (addr >> 10) & 0x01;
-        }
-        throw std::logic_error("Unhandled mirroring scenario");
-    }
-
-    [[nodiscard]] constexpr static auto bank_offset(std::uint16_t addr) noexcept -> std::uint16_t  {
-        return addr & 0x3FF;
-    }
-
-private:
-    using bank = std::array<std::uint8_t, 2_Kb>;
-    std::array<bank, 2> vram_;
-};
 
 class palette_table
 {
