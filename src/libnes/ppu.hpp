cbedc495e6742664df45ee27ff5bd8b861592199
Volodymyr Shamray <shamray@outlook.com>
2021-12-21 09:27:16 +0200

    Move palette_table to dedicated file

--- src/libnes/ppu.hpp
+++ src/libnes/ppu.hpp
@@ -3,6 +3,7 @@
 #include <libnes/literals.hpp>
 #include <libnes/color.hpp>
 #include <libnes/ppu_name_table.hpp>
+#include <libnes/ppu_palette_table.hpp>
 
 #include <array>
 #include <optional>
@@ -42,42 +43,7 @@ private:
 
 
 
-class palette_table
-{
-public:
-    explicit constexpr palette_table(const auto& system_color_palette)
-        : system_colors_{system_color_palette}
-    {
-        palette_ram_.fill(0);
-    }
 
-    [[nodiscard]] static constexpr auto palette_address(std::uint8_t address) noexcept {
-        address &= 0x1F;
-
-        if ((address & 0x13) == 0x10) {
-            address &= ~0x10;
-        }
-
-        return address;
-    }
-
-    [[nodiscard]] constexpr auto read(std::uint8_t address) const noexcept {
-        return palette_ram_[palette_address(address)];
-    }
-
-    constexpr void write(std::uint8_t address, std::uint8_t value) noexcept {
-        palette_ram_[palette_address(address)] = value;
-    }
-
-    [[nodiscard]] auto color_of(std::uint8_t pixel, std::uint8_t palette) const noexcept -> color {
-        auto rpc = pixel ? read((palette << 2) + pixel) : read(0x00);
-        return system_colors_[rpc & 0x3F];
-    }
-
-private:
-    std::array<std::uint8_t, 32> palette_ram_{};
-    const std::array<color, 64>& system_colors_;
-};
 
 struct sprite
 {
