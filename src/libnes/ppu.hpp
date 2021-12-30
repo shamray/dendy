280167b52160ddb95ecf354bbd8671f98b4a51af
Volodymyr Shamray <shamray@outlook.com>
2021-12-30 10:08:19 +0200

    Move pattern table to a dedicated file

--- src/libnes/ppu.hpp
+++ src/libnes/ppu.hpp
@@ -4,6 +4,7 @@
 #include <libnes/color.hpp>
 #include <libnes/ppu_name_table.hpp>
 #include <libnes/ppu_palette_table.hpp>
+#include <libnes/ppu_pattern_table.hpp>
 
 #include <array>
 #include <optional>
@@ -12,38 +13,6 @@
 #include <unordered_map>
 
 namespace nes {
-
-class pattern_table {
-public:
-    using memory_bank = std::array<std::uint8_t, 8_Kb>;
-
-    pattern_table() = default;
-    explicit pattern_table(const memory_bank* bank)
-        : bank_{bank}
-    {}
-
-    constexpr void connect(const memory_bank* new_bank) {
-        bank_ = new_bank;
-    }
-
-    [[nodiscard]] constexpr auto is_connected() const { return bank_ != nullptr; }
-
-    [[nodiscard]] constexpr auto read(std::uint16_t addr) const {
-        if (bank_ == nullptr)
-            throw std::range_error("No CHR bank connected");
-
-        return bank_->at(addr);
-    }
-    constexpr void write(std::uint16_t, std::uint8_t )    {}
-
-private:
-    const memory_bank* bank_{nullptr};
-};
-
-
-
-
-
 
 struct sprite
 {
