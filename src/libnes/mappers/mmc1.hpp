8cd0950fe5f9d2277c1aa937e107b4f6b2db30db
Volodymyr Shamray <shamray@outlook.com>
2022-02-13 21:25:25 +0200

    Add MMC1 registers stub

--- src/libnes/mappers/mmc1.hpp
+++ src/libnes/mappers/mmc1.hpp
@@ -9,6 +9,22 @@
 
 namespace nes {
 
+class mmc1_registers
+{
+    enum class prg_mode: std::uint8_t { switch32k, switch_16k_lo, switch_16k_hi};
+    enum class chr_mode: std::uint8_t { switch8k, switch4k };
+    enum class mirroring_mode: std::uint8_t { one_screen_lo, one_screen_hi, vertical, horizontal};
+
+    constexpr void load(std::uint8_t byte);
+
+    [[nodiscard]] constexpr auto control() const -> std::uint8_t;
+    [[nodiscard]] constexpr auto mirroring() const -> name_table_mirroring;
+
+    [[nodiscard]] constexpr auto chr0() const -> std::uint8_t;
+    [[nodiscard]] constexpr auto chr1() const -> std::uint8_t;
+    [[nodiscard]] constexpr auto prg() const -> std::uint8_t;
+};
+
 class mmc1 final: public cartridge
 {
 public:
--- test/unit_tests/mmc1_test.cpp
+++ test/unit_tests/mmc1_test.cpp
@@ -1,2 +1,7 @@
 #include <libnes/cartridge.hpp>
 #include <catch2/catch.hpp>
+
+TEST_CASE("MMC1 registers") {
+
+
+}
