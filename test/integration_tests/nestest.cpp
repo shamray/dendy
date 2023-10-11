30e2568e59e397e2377bb7c4e89dab4ec753e010
Volodymyr Shamray <shamray@outlook.com>
2023-10-11 18:27:03 +0300

    Refactor nestest

--- test/integration_tests/nestest.cpp
+++ test/integration_tests/nestest.cpp
@@ -1,19 +1,17 @@
 #include <catch2/catch_all.hpp>
+#include <chrono>
 #include <fstream>
 #include <ranges>
-#include <ctime>
-#include <chrono>
 
 #include <libnes/cpu.hpp>
 #include <libnes/literals.hpp>
 
 using namespace nes::literals;
 
-struct test_bus
-{
+struct test_bus {
     void write(std::uint16_t addr, std::uint8_t value) { mem[addr] = value; }
-    std::uint8_t read(std::uint16_t addr) const { return mem[addr]; }
-    bool nmi() const { return false; }
+    [[nodiscard]] std::uint8_t read(std::uint16_t addr) const { return mem[addr]; }
+    [[nodiscard]] bool nmi() const { return false; }
 
     std::vector<std::uint8_t>& mem;
 };
@@ -22,8 +20,7 @@ class test_cpu: public nes::cpu<test_bus>
 {
 public:
     explicit test_cpu(test_bus& bus)
-        : nes::cpu<test_bus>(bus)
-    {
+        : nes::cpu<test_bus>(bus) {
         pc.assign(0xC000);
 
         s.assign(0xFF);
@@ -33,26 +30,23 @@ public:
         p.set(nes::cpu_flag::int_disable);
     }
 
-    [[nodiscard]]
-    auto is_test_finished() const { return pc.value() == 0x1983; }
-
-    std::ostream& print_status(std::ostream& stream)
-    {
+    std::ostream& print_status(std::ostream& stream) {
         return stream << std::hex << std::setw(4) << std::setfill('0') << pc.value() << '\t'
-                      << "A:" << std::setw(2) << std::setfill('0') << (int)a.value() << ' '
-                      << "X:" << std::setw(2) << std::setfill('0') << (int)x.value() << ' '
-                      << "Y:" << std::setw(2) << std::setfill('0') << (int)y.value() << ' '
-                      << "P:" << std::setw(2) << std::setfill('0') << (int)p.value() << ' '
-                      << "SP:" << std::setw(2) << std::setfill('0') << (int)s.value();
+                      << "A:" << std::setw(2) << std::setfill('0') << (int) a.value() << ' '
+                      << "X:" << std::setw(2) << std::setfill('0') << (int) x.value() << ' '
+                      << "Y:" << std::setw(2) << std::setfill('0') << (int) y.value() << ' '
+                      << "P:" << std::setw(2) << std::setfill('0') << (int) p.value() << ' '
+                      << "SP:" << std::setw(2) << std::setfill('0') << (int) s.value();
     }
+
+    [[nodiscard]] auto is_test_finished() const { return pc.value() == 0x1983; }
 };
 
-auto load_nestest()
-{
+auto load_nestest() {
     auto memory = std::vector<std::uint8_t>(64_Kb, 0);
     auto romfile = std::ifstream{"rom/nestest.nes", std::ifstream::binary};
 
-    romfile.seekg(16); // header
+    romfile.seekg(16);// header
 
     auto prg = std::vector<std::uint8_t>(16_Kb, 0);
     romfile.read(reinterpret_cast<char*>(prg.data()), prg.size());
@@ -63,43 +57,36 @@ auto load_nestest()
     return memory;
 }
 
-TEST_CASE("The ultimate NES CPU test ROM (aka nestest)")
-{
+TEST_CASE("The ultimate NES CPU test ROM (aka nestest)") {
     auto m = load_nestest();
     auto bus = test_bus{m};
     auto cpu = test_cpu{bus};
 
     auto log = std::ofstream{"nestest.log"};
-    auto start_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    auto start_time = std::chrono::system_clock::now();
 
     auto instruction_count = 0;
     auto cycle = 0;
 
-    try
-    {
-        auto buffer = std::array<char, 0x100>{};
-        ctime_s(buffer.data(), buffer.size(), &start_time);
-        log << "nestest, test started: " << buffer.data() << '\n' << std::endl;
+    try {
+        log << std::format("nestest, test started: {:%F} {:%T}\n", start_time, std::chrono::floor<std::chrono::seconds>(start_time));
 
-        for (;!cpu.is_test_finished(); ++cycle) {
+        for (; !cpu.is_test_finished(); ++cycle) {
             if (cycle > 10000000)
                 throw std::runtime_error("Probably an infinite loop");
 
             if (!cpu.is_executing()) {
                 cpu.print_status(log) << '\n';
-                instruction_count++;
+                ++instruction_count;
             }
             cpu.tick();
         }
-    }
-    catch(const std::exception& ex)
-    {
+    } catch (const std::exception& ex) {
         FAIL_CHECK(ex.what());
     }
 
-    log << std::endl;
-    log << "test finished, " << std::dec << instruction_count << " instructions, " << cycle << " cycles" << std::endl;
+    log << std::format("\ntest finished, {} instructions, {} cycles\n", instruction_count, cycle);
 
-    CHECK((int)m[0x02] == 0x00);
-    CHECK((int)m[0x03] == 0x00);
+    CHECK((int) m[0x02] == 0x00);
+    CHECK((int) m[0x03] == 0x00);
 }
